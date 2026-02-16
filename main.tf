56c5229d484dad021ba603858f4f08b0725d9f78
hethvikkalyan <162332767+hethvikkalyan@users.noreply.github.com>
2026-02-16 14:14:18 +0530

    Creating main.tf for gcp infra provisioning

---
+++ main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# ----------------------------
+# Network
+# ----------------------------
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "terraform-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+# ----------------------------
+# Firewall Rule (Allow SSH)
+# ----------------------------
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
+# ----------------------------
+# Compute Engine Instance
+# ----------------------------
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 20
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.id
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  service_account {
+    scopes = ["cloud-platform"]
+  }
+}
